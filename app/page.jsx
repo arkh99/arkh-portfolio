71b03832ff368162b84c725af706c19c075660aa
alireza <arkh919@gmail.com>
2024-07-25 23:22:28 -0400

    home page section done, small bug in stats

--- app/page.jsx
+++ app/page.jsx
@@ -2,6 +2,7 @@ import { Button } from "@/components/ui/button";
 import { FiDownload } from "react-icons/fi";
 import Social from "@/components/Social";
 import Photo from "@/components/Photo";
+import Stats from "@/components/Stats";
 
 const Home = () => {
   return (
@@ -41,6 +42,7 @@ const Home = () => {
           </div>
         </div>
       </div>
+      <Stats />
     </section>
   );
 };
--- components/Photo.jsx
+++ components/Photo.jsx
@@ -17,7 +17,7 @@ const Photo = () => {
             opacity: 1,
             transition: { delay: 2.4, duration: 0.4, ease: "easeInOut" },
           }}
-          className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mix-blend-lighten"
+          className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mix-blend-lighten absolute"
         >
           <Image
             src="/assets/photo.png"
@@ -28,6 +28,33 @@ const Photo = () => {
             className="object-contain"
           />
         </motion.div>
+
+        <motion.svg
+          className="w-[300px] xl:w-[506px] h-[300px] xl:h-[506px]"
+          fill="transparent"
+          viewBox="0 0 506 506"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <motion.circle
+            cx="253"
+            cy="253"
+            r="250"
+            stroke="#00ff00"
+            strokeWidth="4"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            initial={{ strokeDasharray: "24 10 0 0" }}
+            animate={{
+              strokeDasharray: ["15 120 25 25", "16 25 92 72", "4 250 22 22"],
+              rotate: [120, 360],
+            }}
+            transition={{
+              duration: 20,
+              repeat: Infinity,
+              repeatType: "reverse"
+            }}
+          />
+        </motion.svg>
       </motion.div>
     </div>
   );
