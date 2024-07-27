49b11f4f3bc1bab17f069c8cda60551229c18dfc
alireza <arkh919@gmail.com>
2024-07-26 23:18:27 -0400

    services done

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -43,13 +43,13 @@ const Services = () => {
                         return (
                             <div key={index} className="flex-1 flex flex-col justify-center gap-6 group">
                             <div className="w-full flex justify-between items-center">
-                                <div className="text-5xl font-extrabold text-outline text-transparent group-hover:text-outline-hover">{service.num}</div>
-                                <Link href={service.href}>
-                                    <BsArrowDownRight />
+                                <div className="text-5xl font-extrabold text-outline text-transparent group-hover:text-outline-hover transition-all duration-500">{service.num}</div>
+                                <Link href={service.href} className="w-[70px] h-[70px] rounded-full bg-white group-hover:bg-accent transition-all duration-500 flex justify-center items-center hover:-rotate-45">
+                                    <BsArrowDownRight  className="text-primary text-3xl"/>
                                 </Link>
                                 </div>
-                                <h2>{service.title}</h2>
-                                <p>{service.description}</p>
+                                <h2 className="text-[42px] fond-bold leading-none text-white group-hover:text-accent transition-all duration-500">{service.title}</h2>
+                                <p className="text-white/60">{service.description}</p>
                                 <div className="border-b border-white/20 w-full"></div>
                         </div>
                         );
