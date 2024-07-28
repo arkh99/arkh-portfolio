6e9176add3793925ed7420fd3bc8967057d217a3
alireza <arkh919@gmail.com>
2024-07-28 17:35:37 -0400

    a bug in work page

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -1,7 +1,85 @@
+"use client";
+import { motion } from "framer-motion";
+import React, { useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import { BsArrowUpRight, BsGithub } from "react-icons/bs";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import Link from "next/link";
+import Image from "next/image";
+import { Github } from "lucide-react";
+const projects = [
+  {
+    num: "01",
+    category: "frontend",
+    title: "project 1",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reprehenderit, odio!",
+    stack: [{ name: "Html 5" }, { name: "Css 3" }, { name: "JavaScript" }],
+    image: "/assets/work/thumb1.png",
+    live: "",
+    github: "https://github.com/arkh99",
+  },
+  {
+    num: "02",
+    category: "frontend",
+    title: "project 2",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reprehenderit, odio!",
+    stack: [{ name: "Html 5" }, { name: "Css 3" }, { name: "JavaScript" }],
+    image: "/assets/work/thumb2.png",
+    live: "",
+    github: "https://github.com/arkh99",
+  },
+  {
+    num: "03",
+    category: "backend",
+    title: "project 3",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reprehenderit, odio!",
+    stack: [{ name: "Html 5" }, { name: "Css 3" }, { name: "JavaScript" }],
+    image: "/assets/work/thumb3.png",
+    live: "",
+    github: "https://github.com/arkh99",
+  },
+  {
+    num: "04",
+    category: "frontend",
+    title: "project 4",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reprehenderit, odio!",
+    stack: [{ name: "Html 5" }, { name: "Css 3" }, { name: "JavaScript" }],
+    image: "/assets/work/thumb1.png",
+    live: "",
+    github: "https://github.com/arkh99",
+  },
+];
+
 const Work = () => {
-    return (
-        <div>work page</div>
-    )
-}
+  const [project, setProject] = useState(projects[0]);
+  return (
+    <motion.section
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      className="min-h-[80vh] flex flex-col justify-center py-12 xl:px-0"
+    >
+      <div className="container mx-auto">
+        <div className="flex flex-col xl:flex-row xl:gap-[30px]">
+          <div className="w-full xl:w-[50%] xl:h-[460px] flex flex-col xl:justify-between order-2 xl:order-none">
+            <div className="text-8xl leading-none font-extabold text-transparent text-outline">
+              {project.num}
+            </div>
+          </div>
+          <div className="w-full xl:w-[50%]">slider</div>
+        </div>
+      </div>
+    </motion.section>
+  );
+};
 
 export default Work;
