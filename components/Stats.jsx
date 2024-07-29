efdbc14eb8cfba4e7355b005ef4432f4fcfadd87
alireza <arkh919@gmail.com>
2024-07-28 22:49:51 -0400

    fix the deployment bug

--- components/Stats.jsx
+++ components/Stats.jsx
@@ -1,5 +1,6 @@
 "use client";
 import CountUp from "react-countup";
+
 const stats = [
   {
     num: 30,
@@ -18,6 +19,7 @@ const stats = [
     text: "Code commits",
   },
 ];
+
 const Stats = () => {
   return (
     <section className="pt-4 pb-12 xl:pt-0 xl:pb-0">
@@ -25,14 +27,16 @@ const Stats = () => {
         <div className="flex flex-wrap gap-6 max-2-[80vw] mx-auto xl:max-w-none">
           {stats.map((item, index) => {
             return (
-              <div className="flex-1 flex gap-4 items-center justify-center xl:justify-start">
+              <div key={index} className="flex-1 flex gap-4 items-center justify-center xl:justify-start">
                 <CountUp
                   end={item.num}
                   duration={5}
                   delay={2}
                   className="text-4xl xl:text-6xl font-extrabold"
-                    />
-                    <p className={`${item.text.length < 15 ? "max-w-[100px]" : "max-2-[150px]"} leading-snug text-white/80`}>{item.text}</p>
+                />
+                <p className={`${item.text.length < 15 ? "max-w-[100px]" : "max-2-[150px]"} leading-snug text-white/80`}>
+                  {item.text}
+                </p>
               </div>
             );
           })}
