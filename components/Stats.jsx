a077139db79eeb38ccaf4e05dc3bcfff8b0b603c
alireza <arkh919@gmail.com>
2024-07-28 22:54:01 -0400

    check

--- components/Stats.jsx
+++ components/Stats.jsx
@@ -2,22 +2,10 @@
 import CountUp from "react-countup";
 
 const stats = [
-  {
-    num: 30,
-    text: "Repository",
-  },
-  {
-    num: 5,
-    text: "Projects completed",
-  },
-  {
-    num: 8,
-    text: "Technologies",
-  },
-  {
-    num: 250,
-    text: "Code commits",
-  },
+  { num: 30, text: "Repository" },
+  { num: 5, text: "Projects completed" },
+  { num: 8, text: "Technologies" },
+  { num: 250, text: "Code commits" },
 ];
 
 const Stats = () => {
@@ -25,21 +13,19 @@ const Stats = () => {
     <section className="pt-4 pb-12 xl:pt-0 xl:pb-0">
       <div className="container mx-auto">
         <div className="flex flex-wrap gap-6 max-2-[80vw] mx-auto xl:max-w-none">
-          {stats.map((item, index) => {
-            return (
-              <div key={index} className="flex-1 flex gap-4 items-center justify-center xl:justify-start">
-                <CountUp
-                  end={item.num}
-                  duration={5}
-                  delay={2}
-                  className="text-4xl xl:text-6xl font-extrabold"
-                />
-                <p className={`${item.text.length < 15 ? "max-w-[100px]" : "max-2-[150px]"} leading-snug text-white/80`}>
-                  {item.text}
-                </p>
-              </div>
-            );
-          })}
+          {stats.map((item, index) => (
+            <div key={index} className="flex-1 flex gap-4 items-center justify-center xl:justify-start">
+              <CountUp
+                end={item.num}
+                duration={5}
+                delay={2}
+                className="text-4xl xl:text-6xl font-extrabold"
+              />
+              <p className={`${item.text.length < 15 ? "max-w-[100px]" : "max-2-[150px]"} leading-snug text-white/80`}>
+                {item.text}
+              </p>
+            </div>
+          ))}
         </div>
       </div>
     </section>
