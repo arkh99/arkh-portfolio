58015ad740a12fac6a76e74419bf4a17c143c47d
alireza <arkh919@gmail.com>
2024-07-28 22:18:48 -0400

    done !!!!!

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -45,7 +45,7 @@ const Contact = () => {
       >
           <div className="container mx-auto">
               <div className="flex flex-col xl:flex-row gap-[30px]">
-                  <div className="xl:h-[54%] order-2 xl:order-none">
+                  <div className="xl:w-[54%] order-2 xl:order-none">
                       <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
                           <h3 className="text-4xl text-accent">Let's work together</h3>
                           <p className="text-white/60">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsam, quo!</p>
@@ -82,9 +82,25 @@ const Contact = () => {
                                   </SelectGroup>
                               </SelectContent>
                           </Select>
+                          <Textarea className="h-[200px]" placeholder="Type your message here." />
+                          <Button size="md" className="max-w-40">Send message</Button>
                       </form>
                   </div>
-                  <div className="flex-1 flex items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0 ">info</div>
+                  <div className="flex-1 flex items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0 ">
+                      <ul className="flex flex-col gap-10">
+                          {info.map((item, index) => {
+                              return <li key={index} className="flex items-center gap-6">
+                                  <div className="w-[52px] h-[52px] xl:w-[72px] xl:h-[72px] bg-[#27272c] text-accent rounded-md flex items-center justify-center">
+                                      <div className="text-[28px]">{item.icon}</div>
+                                  </div>
+                                  <div className="flex-1">
+                                      <p className="text-white/60">{item.title}</p>
+                                      <h3 className="text-xl">{item.description}</h3>
+                                  </div>
+                              </li>
+                          })}
+                      </ul>
+                  </div>
               </div>
           </div>
     </motion.section>
